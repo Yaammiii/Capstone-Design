6bfe6f2d199d7a11ba0d90abd51c80a1a2f84b28
Yaammiii <127203022+Yaammiii@users.noreply.github.com>
2023-06-05 12:27:15 +0900

    Update and rename Control.py to control.py

--- control.py
+++ control.py
@@ -3,21 +3,23 @@ import lidar_scan as ls
 import object_scan as _os
 ####################################
 import cv2
-from multiprocessing import Process, Queue
-import rclpy
-from std_msgs.msg._u_int8_multi_array import UInt8MultiArray
-from std_msgs.msg._float32_multi_array import Float32MultiArray
-from ultralytics import YOLO
+import numpy as np
+import matplotlib.pyplot as plt
 from time import sleep
 import time
 import random
-import time
+from ultralytics import YOLO
+from multiprocessing import Process, Queue
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg._u_int8_multi_array import UInt8MultiArray
+from std_msgs.msg._float32_multi_array import Float32MultiArray
 from rclpy.qos import qos_profile_sensor_data, QoSProfile, HistoryPolicy, ReliabilityPolicy, DurabilityPolicy
-from sensor_msgs.msg import Image as msg_Image
-import cv2
-import numpy as np
+from sensor_msgs.msg import Image as msg_Image, LaserScan
+
+
+
+is_lidar_triggerred = True
 
 class RSPi_Subsriber(Node):
     def __init__(self, 
@@ -56,36 +58,67 @@ def send_ctrl_info(
         buffer_Brake_Yolo,  # Braking data from Yolo
         buffer_Brake_Lidar  # braking data from Lidar
     ):
+    global is_lidar_triggerred
     rclpy.init()
-    node = rclpy.create_node('PC_node1')
-    publisher = node.create_publisher(Float32MultiArray, 'steer_info1', 5)
+    node = rclpy.create_node('PC_node')
+    publisher = node.create_publisher(Float32MultiArray, 'steer_info', 10)
     msg = Float32MultiArray()
     servo_angle = 90
     motor_duty = 5
+    s_time = time.time()
+    
 
     while True:
-        motor_duty = 15
+        motor_duty = 15.5
         if not buffer_Handle.empty():
             servo_angle = buffer_Handle.get()
         if abs(servo_angle-90)> 2:
-            motor_duty = 12.7
+            motor_duty = 16.1
+        if abs(servo_angle-90)>= 5:
+            motor_duty = 16.8
+        if abs(servo_angle-90)> 7:
+            motor_duty = 18
+        if abs(servo_angle-90)> 10:
+            motor_duty = 21
+        if abs(servo_angle-90)>= 15:
+            motor_duty = 25
+        if abs(servo_angle-90)> 18:
+            motor_duty = 28
+        if abs(servo_angle-90)> 20:
+            motor_duty = 30
         
-        if not buffer_Brake_Yolo.empty():
-            is_stop = buffer_Brake_Yolo.get()
-            motor_duty=0
+        if servo_angle <= 60:
+            servo_angle =60
+        if servo_angle >= 120:
+            servo_angle = 120
         #motor_duty=0
 
+        if round(3*time.time()) %3 ==0:
+            motor_duty = 5
+        
+        if not buffer_Brake_Lidar.empty():
+            if buffer_Brake_Lidar.get():
+                s_time = time.time()
+
+        
+        
+        if not buffer_Brake_Yolo.empty():
+            if buffer_Brake_Yolo.get():
+                s_time = time.time()
+
+        if time.time()-s_time<2:
+            motor_duty=0
         
         msg.data = [float(motor_duty), float(servo_angle)]
         publisher.publish(msg)
-        sleep(0.3)
+        sleep(0.1)
         print('msg sent:', [motor_duty, servo_angle])
 
 def lane(region, buffer_Handle, buffer_Front_Img):
     rclpy.init(args=None)
 
-    sub = RSPi_Subsriber(node_name = 'RSPiSub_Node1',
-                        topic_name = 'Lane_Img1'
+    sub = RSPi_Subsriber(node_name = 'RSPiSub_Node',
+                        topic_name = 'Lane_Img'
                         )
     print('Start Subscribing')
 
@@ -110,16 +143,14 @@ def lane(region, buffer_Handle, buffer_Front_Img):
         received_msg = sub.get_data()
         img = np.array(received_msg.data)
         merged_img = np.reshape(img, (240, 960, 3))
-        frame_R = merged_img[:, 0:320, :]
-        frame_Front = merged_img[:, 320:640, :]
-        frame_L = merged_img[:, 640:960, :]
+        frame_Front = merged_img[:, 0:320, :]
+        frame_L = merged_img[:, 320:640, :]
+        frame_R = merged_img[:, 640:960, :]
 
 
         if buffer_Front_Img.full():
             buffer_Front_Img.get()
         buffer_Front_Img.put(frame_Front)
-
-        continue
 
         frame_L = cv2.rotate(frame_L, cv2.ROTATE_90_COUNTERCLOCKWISE)
         frame_R = cv2.rotate(frame_R, cv2.ROTATE_90_CLOCKWISE)
@@ -131,14 +162,14 @@ def lane(region, buffer_Handle, buffer_Front_Img):
         except:
             print('Left side Pipeline error')
             img_L, curvature_L, left_fit_L, angle_L = prev_L1, prev_L2, prev_L3, prev_L4
-            curvature_L=0
+            curvature_L=90
         
         try:
             img_R, curvature_R, left_fit_R, angle_R = ld.pipeline(frame_R, region, curvature_R, left_fit_R,'R')
         except:
             print('Right side PipeLine error')
             img_R, curvature_R, left_fit_R, angle_R = prev_R1, prev_R2, prev_R3, prev_R4
-            curvature_R=0
+            curvature_R=90
         print('img_L:', round(curvature_L, 1), end='\t')
         print('img_R:', round(curvature_R, 1))
         prev_L1, prev_L2, prev_L3, prev_L4 = img_L, curvature_L, left_fit_L, angle_L
@@ -166,9 +197,8 @@ def lane(region, buffer_Handle, buffer_Front_Img):
         #print("Put Handle Angle data to buffer")
 
 
-
 def obj_detection(buffer_Front_Img, buffer_Brake_Yolo, buffer_Slowdown):
-    model = YOLO(r"/home/hanwool/Desktop/integrate/best_25.pt")
+    model = YOLO(r"/media/hanwool/USB/integrate1/integrate/best_25.pt")
     class_list = list(model.names.values())
     print('model Loaded')
 
@@ -188,7 +218,6 @@ def obj_detection(buffer_Front_Img, buffer_Brake_Yolo, buffer_Slowdown):
 
     while True:
         if buffer_Front_Img.empty():
-            #print('buffer front empty')
             continue
         frame = buffer_Front_Img.get()
         data_dict, frame = _os.object_detect(model, 
@@ -197,14 +226,13 @@ def obj_detection(buffer_Front_Img, buffer_Brake_Yolo, buffer_Slowdown):
                                              class_list)
         cv2.imshow('ObjectDetection', frame)
         detected_obj = [key for key in data_dict.keys()]
-        #print(detected_obj)
         if not is_stopped:
             brake_time = time.time()
         if len(detected_obj) == 0:
             pass
         else:
             for obj in detected_obj:
-                if obj == 'Trafficlight':
+                if obj == 'trafficlight':
                     if buffer_Brake_Yolo.full():
                             buffer_Brake_Yolo.get()
                     if data_dict[obj]:
@@ -227,7 +255,7 @@ def obj_detection(buffer_Front_Img, buffer_Brake_Yolo, buffer_Slowdown):
                             buffer_Brake_Yolo.get()
                         buffer_Brake_Yolo.put(False)
                         is_stopped = False
-                elif obj == 'Person':
+                elif obj == 'person':
                     if buffer_Brake_Yolo.full():
                             buffer_Brake_Yolo.get()
                     if data_dict[obj]:
@@ -243,6 +271,30 @@ def obj_detection(buffer_Front_Img, buffer_Brake_Yolo, buffer_Slowdown):
                         buffer_Slowdown.put(False)
         cv2.waitKey(1)
 
+
+def lidar_info(buffer_Brake_Lidar):
+    def callback(msg):
+        x, y, is_true = ls.draw(msg)
+        plt.figure(1)
+        plt.cla()
+        plt.ylim(-500,500)
+        plt.xlim(-500,500)
+        plt.scatter(x, y, c='r', s=13)
+        plt.title('Lidar Warning - Hanwool Lee')
+        #print(q)
+        #plt.scatter(q, r, c='b', s=13)
+        plt.pause(0.001)
+        #plt.close("all")
+        if buffer_Brake_Lidar.full():
+            buffer_Brake_Lidar.get()
+        buffer_Brake_Lidar.put(is_true)
+
+    rclpy.init()
+    node = rclpy.create_node('LiDAR_Sub')
+    node.create_subscription(LaserScan, '/scan', callback, qos_profile_sensor_data)
+    # Wait for messages to arrive
+    while rclpy.ok():
+        rclpy.spin_once(node)
 if __name__ == "__main__":
     # line detecting area
     top_left = [0, 0]
@@ -258,13 +310,13 @@ if __name__ == "__main__":
     buffer_Front_Img = Queue(maxsize=5)
 
     servo_angle = 90
-    #motor_duty = 0
+    motor_duty = 0
     buffer_Handle.put(90)
 
     # process spawning
     p_Lane = Process(target=lane, args=(region, buffer_Handle, buffer_Front_Img,))
     p_Yolo = Process(target=obj_detection, args=(buffer_Front_Img, buffer_Brake_Yolo, buffer_Slowdown,))
-    #p_Lidar= Process()
+    #p_Lidar= Process(target=lidar_info, args=(buffer_Brake_Lidar,))
     p_Ctrl = Process(
         target=send_ctrl_info, 
         args=(buffer_Handle,        # Handling data from Lane Detection
@@ -276,13 +328,15 @@ if __name__ == "__main__":
     p_Lane.start()
     p_Ctrl.start()
     p_Yolo.start()
+    #p_Lidar.start()
     try:
         while True:
             pass
 
     except:
         rclpy.shutdown()  
-        #p_Lane.kill()
+        p_Lane.kill()
         p_Ctrl.kill()
-        #p_Yolo.kill()
+        #p_Lidar.kill()
+        p_Yolo.kill()
        
